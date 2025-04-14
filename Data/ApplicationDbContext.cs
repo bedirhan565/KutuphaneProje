4c4d97cd00ecb23c7332ac46a558512b285d0fcd
bedirhan565 <emirhanbry445@gmail.com>
2025-04-14 09:56:19 +0300

    fistcommit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+using Kutuphane.Models;
+using Kutuphane.Data;
+using Microsoft.EntityFrameworkCore;
+namespace Kutuphane.Data{
+public class ApplicationDbContext : DbContext{
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Sinif> Siniflar{ get; set; }
+}
+}
---
+++ Models/Ogrenci.cs
@@ -0,0 +1,10 @@
+namespace Kutuphane.Models;
+
+public class Ogrenci{
+    public int Id {get; set;}
+    public string Ad {get; set;}
+    public string Soyad {get; set;}
+    public int Numara {get; set;}
+    public int SinifId {get; set;}
+    public int Sinif {get; set;}
+}
